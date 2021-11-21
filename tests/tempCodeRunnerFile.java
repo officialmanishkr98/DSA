8a176d0df7cf2a4db37d2d5d22191ce06ef9ec4c
Manish <officialmanishkr98@gmail.com>
2021-11-21 20:15:34 +0530

    first commit

--- tests/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-f(i==2)
-            // {
-            //     System.out.print(n +" ");
-            // }
-            // if(i==4)
-            // {
-            //     System.out.print(n+" ");
-            // }
